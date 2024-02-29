9ac9c2b6273ed298e731babbb6378cae3eb25e06
gleblobanov08 <135247482+gleblobanov08@users.noreply.github.com>
2024-02-29 16:20:26 +0300

    changes

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,9 +1,62 @@
 const cells = document.getElementsByClassName("cell");
+const results = document.getElementById("results");
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 let currentPlayer = 'X';
 let gameOver = false;
 
-function resetGame() {
-    // field reset
+startGame();
+
+function startGame() {
+    cells.forEach(el => {
+        el.addEventListener('click', makeMove(el), {once = true})
+    });
 }
 
-document.getElementById("reset-btn").addEventListener('click', resetGame());
+function makeMove(el) {
+    const spot = el.target;
+    placeSpot(spot, currentPlayer)
+    if (!checkEnd) {
+        changePlayer();
+    }
+}
+
+function changePlayer () {
+    if (currentPlayer === 'X') {
+        currentPlayer = 'O';
+    } else {
+        currentPlayer = 'X';
+    }
+}
+
+function placeSpot(square, player) {
+    square.innerText = player;
+}
+
+function checkEnd () {
+    if (winningCombinations.some(combination => {
+        return combination.every(i => {
+            return cells[i].innerText.contains(currentPlayer)
+        })
+    })) {
+        results.innerText = currentPlayer + " won!";
+        gameOver = true;
+    } else if ([...cells].every(el => {
+        el.innerText.contains("X") || el.innerText.contains("O");
+    })) {
+        results.innerText = "Draw!";
+        gameOver = true;
+    } else {
+        continue;
+    }
+}
+
+document.getElementById("reset-btn").addEventListener("click", startGame());
