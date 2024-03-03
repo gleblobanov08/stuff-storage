007c7535bd24b033a462d29348be935c36b716ec
gleblobanov08 <135247482+gleblobanov08@users.noreply.github.com>
2024-03-03 09:31:42 +0300

    finish

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,55 +1,80 @@
-const cells = Array.from(document.getElementsByClassName("cell"));
 const results = document.getElementById("results");
 const winningCombinations = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
 ];
-let currentPlayer = 'X';
-let spaces = Array(9).fill(null);
+let currentPlayer = "X";
+let gameDead = false;
+let spaces = ["", "", "", "", "", "", "", "", "", ""];
 
-const startGame = () => {
-    cells.forEach(el => el.addEventListener('click', makeMove))
-};
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', makeMove));
+document.getElementById("reset-btn").addEventListener('click', restartGame);
 
 function makeMove(el) {
-    const spot = el.target.id;
-    if (!spaces[spot]) {
-        spaces[spot] = currentPlayer;
-        el.target.innerText = currentPlayer;
-
-        currentPlayer = currentPlayer === "X" ? "O" : "X";
-
-        if (checkEnd() !== false) {
-            results.innerText = currentPlayer + " has won!";
-        }
+    const clicked = el.target;
+    const clickedIndex = parseInt(clicked.getAttribute("id"));
+  
+    if (spaces[clickedIndex] !== "" || gameDead) {
+        return;
     }
+  
+    movePlayed(clicked, clickedIndex);
+    checkEnd();
+ }
+
+function movePlayed(cell, cellIndex) {
+    spaces[cellIndex] = currentPlayer;
+    cell.innerText = currentPlayer;
 }
 
-function checkEnd () {
-    for (const condition in winningCombinations) {
-        let [a, b, c] = condition;
-
-        if (spaces[a] && (spaces[a] === spaces[b] === spaces[c])) {
-            return [a, b, c];
+function checkEnd() {
+    let gameOver = false;
+    for (let i = 0; i < 8; i++) {
+        const condition = winningCombinations[i];
+        let a = spaces[condition[0]];
+        let b = spaces[condition[1]];
+        let c = spaces[condition[2]];
+    
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+    
+        if (a === b && b === c) {
+            gameOver = true;
+            break;
         }
     }
-    return false;
+
+    if (gameOver) {
+        results.innerText = currentPlayer + ' has won!';
+        gameDead = true;
+        return;
+    }
+
+    let staleMate = !spaces.includes("");
+    if (staleMate) {
+        results.innerText = "Is' a draw!";
+        gameDead = true;
+        return;
+    }
+  
+    changePlayer();
 }
 
-function restart() {
-    spaces.fill(null);
-    cells.forEach(el => {
-        el.innerText = "";
-    });
+function changePlayer () {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+}
+
+function restartGame () {
+    gameDead = false;
+    spaces = ["", "", "", "", "", "", "", "", ""];
     currentPlayer = "X";
     results.innerText = "";
-};
-document.getElementById("reset-btn").addEventListener("click", restart());
-
-startGame();
+    document.querySelectorAll(".cell").forEach(cell => cell.innerText = "");
+}
