379ec63efafe190d2cd1e7e72219f2dbbbf98fd1
gleblobanov08 <135247482+gleblobanov08@users.noreply.github.com>
2024-05-21 21:42:14 +0300

    ugly slot machine

---
+++ slot-machine/script.js
@@ -0,0 +1,26 @@
+const possibleOutcomes = ["1", "2", "3", "4", "5"];
+
+const rings = document.querySelectorAll(".ring");
+rings.forEach(ring => ring.innerText = "?");
+
+const btn = document.getElementById("spin");
+btn.addEventListener('click', spin);
+
+function spin() {
+  for (let i = 0; i < 3; i++) {
+    setTimeout(() => { 
+        rings[i].innerText = possibleOutcomes[Math.floor(Math.random() * 5)];
+    }, i * 800);
+  }
+  checkWin();
+}
+
+function checkWin () {
+  const a = document.getElementById("ring1");
+  const b = document.getElementById("ring2"); const c = document.getElementById("ring3");
+  if (a.innerText === b.innerText && b.innerText == c.innerText && a.innerText !== "?") {
+    document.getElementById("info").innerText = "You Win!";
+  } else {
+    rings.forEach(ring => ring.innerText = "?");
+  }
+}
