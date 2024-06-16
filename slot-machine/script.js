1c2eb242d1770e0fa847efcb37ac9859404b7b65
gleblobanov08 <135247482+gleblobanov08@users.noreply.github.com>
2024-06-16 18:06:37 +0300

    v.2.0 - infinite spin fixed

--- slot-machine/script.js
+++ slot-machine/script.js
@@ -1,26 +1,38 @@
-const possibleOutcomes = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
+const possibleOutcomes = ["1", "2", "3", "4", "5"];
 const rings = document.querySelectorAll(".ring");
 rings.forEach(ring => ring.innerText = "?");
 
 const btn = document.getElementById("spin");
 btn.addEventListener('click', spin);
+let btnIsClickable = true;
 
 function spin() {
-  for (let i = 0; i < 3; i++) {
-    setTimeout(() => { 
-        rings[i].innerText = possibleOutcomes[Math.floor(Math.random() * 9)];
-    }, i * 800);
+  if (btnIsClickable) {
+    rings.forEach(ring => ring.innerText = "?");
+    btnIsClickable = false;
+    btn.classList.add('not-spinable');
+    
+    for (let i = 0; i < 3; i++) {
+      setTimeout(() => {
+        rings[i].innerText = possibleOutcomes[Math.floor(Math.random() * 5)];
+        
+        if (i === 2) {
+          setTimeout(checkWin, 800);
+        }
+      }, i * 800);
+    }
   }
-  checkWin();
 }
 
-function checkWin () {
+function checkWin() {
   const a = document.getElementById("ring1");
-  const b = document.getElementById("ring2"); const c = document.getElementById("ring3");
-  if (a.innerText === b.innerText && b.innerText == c.innerText && a.innerText !== "?") {
+  const b = document.getElementById("ring2");
+  const c = document.getElementById("ring3");
+  
+  if (a.innerText === b.innerText && b.innerText === c.innerText && a.innerText !== "?") {
     document.getElementById("info").innerText = "You Win!";
   } else {
-    rings.forEach(ring => ring.innerText = "?");
+    btn.classList.remove('not-spinable');
+    btnIsClickable = true;
   }
 }
