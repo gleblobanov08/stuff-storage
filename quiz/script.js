29adfca0e8304f5dca40255cf30bae3f80f9796d
gleblobanov08 <135247482+gleblobanov08@users.noreply.github.com>
2024-07-24 21:56:45 +0300

    sync

--- quiz/script.js
+++ quiz/script.js
@@ -4,18 +4,8 @@ async function getData() {
     const table = await data.text();
             
     const tournament = table.split('\n').slice(1);
-    //tournament.forEach(el => console.log(el.split(',')));
     for (let i = 0; i < tournament.length; i++) {
         const element = tournament[i].split(',');
-        /*const year = document.createElement('div');
-        year.textContent = element[0];
-        const place = document.createElement('div');
-        place.textContent = element[1];
-        const event = document.createElement('div');
-        event.textContent = element[2];
-        const champ = document.createElement('div');
-        champ.textContent = element[3];*/
-        //console.log(year, place, event, champ);
         for (let i = 0; i < element.length; i++) {
             const info = document.createElement('div');
             info.textContent = element[i].replaceAll('"', '');
